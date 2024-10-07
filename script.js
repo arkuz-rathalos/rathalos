c51d80ff97fd42ef551f7612e5a1d023cecc181f
Fernando Espada <fespada@dyl.com>
2024-10-07 09:44:26 -0700

    firebase db test-2
    test 2 for firebase db connection

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const db = firebase.firestore();
 
-db.collection("connectiontest").get().then((querySnapshot) => {
+db.collection("dummy").get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
         console.log(`${doc.id} => ${doc.data()}`);
         // You can append this data to your HTML
