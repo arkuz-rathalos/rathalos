148c58be8ce4d58fa95c91a0f38876711025c75b
Fernando Espada <fespada@dyl.com>
2024-10-07 14:52:51 -0700

    firebase document test-3
    + added dropdown
    + table displays selected option

--- script.js
+++
@@ -1,8 +0,0 @@
-const db = firebase.firestore();
-
-db.collection("dummy").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
-        // You can append this data to your HTML
-    });
-});
