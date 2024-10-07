4c04ea8de4a2b85d6f7d783df4062feaa9c61cf2
Fernando Espada <fespada@dyl.com>
2024-10-07 09:37:41 -0700

    firebase db test-1
    + test 1 for firebase db connection

---
+++ script.js
@@ -0,0 +1,8 @@
+const db = firebase.firestore();
+
+db.collection("connectiontest").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+        // You can append this data to your HTML
+    });
+});
